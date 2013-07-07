a523d6198b5692ea3e918f9c36a167a85cebb05a
viappco <viappco@gmail.com>
2013-07-07 20:00:50 +0000

    updated web.js to read index

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var txt = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(txt);
+
 });
 
 var port = process.env.PORT || 5000;
